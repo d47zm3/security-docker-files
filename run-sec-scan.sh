f32c7495dfff9d9603013c7f31a5d9f6e1509397
Damian Tykałowski <d47zm3@gmail.com>
2018-08-21 13:04:34 +0200

    some initial tools

--- run-sec-scan.sh
+++ run-sec-scan.sh
@@ -30,7 +30,7 @@ docker run --rm -t owasp/zap2docker-weekly zap-baseline.py -t ${target}
 
 # arachni - this one takes lots of time, commented out for now
 # mkdir -p $PWD/reports $PWD/artifacts;
-# docker run  -v $PWD/reports:/arachni/reports ahannigan/docker-arachni bin/arachni ${target} --report-save-path=reports/components.gcp.stp.nwt.se.afr;
+# docker run  -v $PWD/reports:/arachni/reports ahannigan/docker-arachni bin/arachni ${target} --report-save-path=reports/domain.com.afr;
 # docker run --name=arachni_report -v $PWD/reports:/arachni/reports ahannigan/docker-arachni bin/arachni_reporter reports/${domain_name}.afr --reporter=html:outfile=reports/${domain_name}-report.html.zip;
 # docker cp arachni_report:/arachni/reports/${domain_name}-report.html.zip $PWD/artifacts;
 # docker rm arachni_report;
