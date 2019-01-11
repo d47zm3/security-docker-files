46569f07251a3c6285c7517e82a3ceeb015275b8
Damian Tykałowski <d47zm3@gmail.com>
2019-01-11 17:17:38 +0100

    redirect logs, show output on error

--- _scripts_/build-all.sh
+++ _scripts_/build-all.sh
@@ -2,6 +2,8 @@
 
 ignore="maltego"
 
+error=0
+
 cd ..
 cwd=$( pwd )
 # show build logs, set "-q" to hide output, empty to show it
@@ -14,12 +16,24 @@ do
   then
     image_name=$( cat Dockerfile  | grep image_name | grep -o '".*"' | sed 's/"//g' )
     echo "[*] $(date +'%H:%M:%S') building ${image_name}..."
-    docker build ${debug} -t ${image_name} -f Dockerfile .
+    docker build ${debug} -t ${image_name} --no-cache -f Dockerfile . > ${image_name}.build.log
+    if [[ ${?} -ne 0 ]]
+    then
+      error=1
+      echo "[*] $(date +'%H:%M:%S') building ${image_name} failed! log is shown below..."
+      cat ${image_name}.build.log
+    fi
     cd ${cwd}
   else
     echo "[*] $(date +'%H:%M:%S') starting build script..."
     chmod +x ./build.sh
-    ./build.sh
+    ./build.sh > ${dir}.build.log
+    if [[ ${?} -ne 0 ]]
+    then
+      error=1
+      echo "[*] $(date +'%H:%M:%S') building custom image from ${dir} failed! log is shown below..."
+      cat ${dir}.build.log
+    fi
     cd ${cwd}
   fi
 done
