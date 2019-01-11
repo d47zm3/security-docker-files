74cb1d99e62aa83595d4b5c793915dca9377e443
Damian Tykałowski <d47zm3@gmail.com>
2019-01-11 17:38:50 +0100

    improve logging on custom amass build
    Signed-off-by: Damian Tykałowski <d47zm3@gmail.com>

--- amass/build.sh
+++ amass/build.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
 git clone https://github.com/OWASP/Amass temporary
 cd temporary
-docker build -t security-tools:amass .
+docker build -t security-tools:amass . > amass.build.log
+if [[ ${?} -ne 0 ]]
+then
+  echo "[ERROR] amass failed to build! log shown below!"
+  cat amass.build.log
+  exit 1
+fi
+
